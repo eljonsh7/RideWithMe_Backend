266e6a384b1a9c7382496fa99b9cb0acb1ec1b1d
endritgjoka <70038985+endritgjokaj@users.noreply.github.com>
2024-03-18 13:39:33 +0100

    User id not returned as UUID, fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
     use HasFactory,HasApiTokens;
 
     protected $primaryKey = 'id';
+    public $incrementing = false;
 
     protected $fillable = [
         'id', 'first_name', 'last_name', 'email', 'password', 'role',
